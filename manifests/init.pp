338663da6f80302673eb3260a9b7eed7618446e7
Jeff McCune <jeff@puppetlabs.com>
2011-05-27 13:59:21 -0700

    Fix whitespace and start to fix class parameters.
    Make sure to write good commit messages for each change.  This doesn't
    count.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,19 +4,19 @@
 #
 # Parameters:
 #
-#  [*version*]            - The version of the MCollective package(s) to 
+#  [*version*]            - The version of the MCollective package(s) to
 #                             be installed.
-#  [*server*]             - Boolean determining whether you would like to 
+#  [*server*]             - Boolean determining whether you would like to
 #                             install the server component.
-#  [*server_config*]      - The content of the MCollective server configuration 
+#  [*server_config*]      - The content of the MCollective server configuration
 #                             file.
-#  [*server_config_file*] - The full path to the MCollective server 
+#  [*server_config_file*] - The full path to the MCollective server
 #                             configuration file.
-#  [*client*]             - Boolean determining whether you would like to 
+#  [*client*]             - Boolean determining whether you would like to
 #                             install the client component.
-#  [*client_config*]      - The content of the MCollective client configuration 
+#  [*client_config*]      - The content of the MCollective client configuration
 #                             file.
-#  [*client_config_file*] - The full path to the MCollective client 
+#  [*client_config_file*] - The full path to the MCollective client
 #                             configuration file.
 #  [*pkg_provider*]       - The package provider resource to use.
 #  [*stomp_server*]       - The hostname of the stomp server.
@@ -33,23 +33,25 @@
 #
 # Sample Usage:
 #
+# The module works with sensible defaults:
+#
 # node default {
-#   class { 'mcollective': 
-#     version             => '1.2.0',
+#   include mcollective
+# }
+#
+# These defaults are:
+#
+# node default {
+#   class { 'mcollective':
+#     version             => 'present',
 #     server              => true,
 #     server_config       => template('mcollective/server.cfg.erb'),
 #     server_config_file  => '/etc/mcollective/server.cfg',
 #     client              => true,
-#     client_config       => template('mcollective/client.cfg.erb'),  
+#     client_config       => template('mcollective/client.cfg.erb'),
 #     client_config_file  => '/home/mcollective/.mcollective',
 #     pkg_provider        => 'yum',
 #     stomp_server        => 'stomp',
-#     stomp_ip            => '10.13.0.3',
-#     stomp_aliases       => '[
-#                               'stomp.vagrant.internal',
-#                               'aserver.vagrant.internal',
-#                               'aserver',
-#                             ],    
 #   }
 # }
 #
@@ -66,7 +68,7 @@ class mcollective(
   $stomp_ip              = $mcollective::params::stomp_ip,
   $stomp_aliases         = $mcollective::params::stomp_aliases
 ) inherits mcollective::params {
-  
+
   $v_bool = [ '^true$', '^false$' ]
   $provider_ensure = [ '^yum$', '^aptitude$', '^pkgdmg$', '^appdmg$' ]
   validate_re($server_config_file, '^/')
@@ -75,8 +77,8 @@ class mcollective(
   validate_re("$client", $v_bool)
   validate_re($pkg_provider, $provider_ensure)
   validate_re($version, '^[._0-9a-zA-Z:-]+$')
-  
-  
+
+
   $server_real               = $server
   $client_real               = $client
   $client_config_file_real   = $client_config_file
@@ -87,15 +89,15 @@ class mcollective(
   $stomp_server_real         = $stomp_server
   $stomp_ip_real             = $stomp_ip
   $stomp_aliases_real        = $stomp_aliases
-  
-  
-  
+
+
+
   if $version == 'UNSET' {
       $version_real = 'present'
   } else {
       $version_real = $version
   }
-  
+
   if $server_real {
     class { 'mcollective::server::base':
       version        => $version_real,
@@ -104,7 +106,7 @@ class mcollective(
       config_file    => $server_config_file_real,
     }
   }
-  
+
   if $client_real {
     class { 'mcollective::client::base':
       version        => $version_real,
